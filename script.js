5ea135a56e6c014f6b162a580d6ec10e11d4ad5b
S_a_b_b_i_R <sabbirssmc90@gmail.com>
2024-03-03 15:53:14 +0600

    dynamic posts and read section done

--- script.js
+++ script.js
@@ -1,1 +1,89 @@
 console.log("connected!");
+const loadPosts = async () => {
+    const res = await fetch(
+        "https://openapi.programming-hero.com/api/retro-forum/posts"
+    );
+    const data = await res.json();
+    const posts = data.posts;
+    const postContainer = document.getElementById("posts");
+    posts.forEach((post) => {
+        const postDiv = document.createElement("div");
+        postDiv.innerHTML = `
+        <div class="bg-cardBg p-10 rounded-xl flex flex-col lg:flex-row justify-start items-start gap-6 hover:cursor-pointer post">
+                    <div class="indicator">
+                        <span class="indicator-item badge ${
+                            post.isActive ? "badge-success" : "badge-error"
+                        }"></span>
+                        <div class="grid w-20 h-20 bg-base-300 place-items-center">
+                            <div class="avatar">
+                                <div class="w-full rounded">
+                                    <img src="${post.image}" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="space-y-5 w-full">
+                        <div class="flex gap-5 inter text-sm font-medium leading-4 text-cardTop">
+                            <p class="">#Music</p>
+                            <p>Author : ${post.author.name}</p>
+                        </div>
+                        <h1 class="mulish text-xl font-bold leading-6">${
+                            post.title
+                        }</h1>
+                        <p class="inter text-base font-normal leading-6 text-normalText">${
+                            post.description
+                        }</p>
+                        <hr class="border-2 border-dashed">
+                        <div class="flex justify-between items-center text-lg">
+                            <div class="flex gap-5 text-normalText">
+                                <div>
+                                    <i class="fa-regular fa-message" style="color: #000000;"></i>
+                                    <span>${post.comment_count}</span>
+                                </div>
+                                <div>
+                                    <i class="fa-regular fa-eye" style="color: #000000;"></i>
+                                    <span>${post.view_count}</span>
+                                </div>
+                                <div>
+                                    <i class="fa-regular fa-clock" style="color: #000000;"></i>
+                                    <span>${post.posted_time} min</span>
+                                </div>
+                            </div>
+                            <div>
+                                <i class="fa-solid fa-envelope" style="color: #30c054;"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        `;
+        postContainer.appendChild(postDiv);
+    });
+    const cards = document.querySelectorAll(".post");
+    for (let card of cards) {
+        card.addEventListener("click", function () {
+            const readItems = document.querySelector("#read");
+            const div = document.createElement("div");
+            div.className =
+                "flex justify-between items-center bg-white p-4 rounded-xl mb-3";
+            const li = document.createElement("li");
+            li.className = "mulish text-lg font-bold w-2/3";
+            const name = card.childNodes[3].childNodes[3].innerText;
+            li.appendChild(document.createTextNode(name));
+            li.style.listStyle = "none";
+            div.appendChild(li);
+            const i = document.createElement("i");
+            i.className = "fa-regular fa-eye text-sm";
+            const span = document.createElement("span");
+            span.className = "text-sm mulish";
+            span.innerText =
+                card.childNodes[3].childNodes[9].childNodes[1].childNodes[3].childNodes[3].innerText;
+            i.appendChild(span);
+            div.appendChild(i);
+            readItems.appendChild(div);
+            const totalRead = document.querySelector("#total-read");
+            totalRead.innerText = readItems.childElementCount;
+        });
+    }
+};
+
+loadPosts();
